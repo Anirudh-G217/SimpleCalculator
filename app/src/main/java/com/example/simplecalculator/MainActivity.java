9e24ede6eecbe12a990067fb1d70351a103816df
Anirudh-G217 <53647649+Anirudh-G217@users.noreply.github.com>
2021-09-05 20:55:08 +0530

    Initial commit

---
+++ app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.simplecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText window;
+    String number1 = "", number2 = "", number = "", operator = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        window = findViewById(R.id.calc_window);
+    }
+
+    public void numberClicked(View view) {
+        Button numberButton = (Button) view;
+        number += numberButton.getText().toString();
+        window.setText(window.getText().toString() + numberButton.getText().toString());
+    }
+
+    public void operatorClicked(View view) {
+        number1 = number;
+        number = "";
+        Button operatorButton = (Button) view;
+        operator = operatorButton.getText().toString();
+        window.setText(window.getText().toString() + operator);
+    }
+
+    public void equalClicked(View view) {
+        try {
+            number2 = number;
+            BigDecimal result = new BigDecimal("0");
+            BigDecimal num1 = new BigDecimal(number1);
+            BigDecimal num2 = new BigDecimal(number2);
+
+            switch (operator) {
+                case "+":
+                    result = num1.add(num2);
+                    break;
+                case "-":
+                    result = num1.subtract(num2);
+                    break;
+                case "*":
+                    result = num1.multiply(num2);
+                    break;
+                case "/":
+                    result = num1.divide(num2,3, RoundingMode.CEILING);
+                    break;
+            }
+            window.setText(String.valueOf(result));
+            number = String.valueOf(result);
+            number1 = "";
+            number2 = "";
+            operator = "";
+        } catch (Exception e) {
+            window.setText(R.string.invalid_expression);
+        }
+
+    }
+
+    public void clearClicked(View view) {
+        number = "";
+        number1 = "";
+        number2 = "";
+        operator = "";
+        window.setText("");
+    }
+}
